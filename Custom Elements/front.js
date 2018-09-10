f8609aa5f4b67a90f6a5ce286842e6fe393d7bcf
gara501 <gandres.ramirez@gmail.com>
2018-09-10 00:06:34 -0500

    New code

--- Custom Elements/front.js
+++
@@ -1,4 +0,0 @@
-var controls = document.querySelectorAll('profile-box');
-controls[0].addEventListener('click', (ev)=> {
-  ev.target.setAttribute('class', 'master');
-});
--- Shadow Dom/shadow.js
+++
@@ -1,7 +0,0 @@
-const header = document.createElement('header');
-const shadowRoot = header.attachShadow({mode: 'open'});// create a shadow DOM for an element
-shadowRoot.innerHTML = '<h1>Hello Shadow DOM</h1>'; // Could also use appendChild().
-
-
-// header.shadowRoot === shadowRoot
-// shadowRoot.host === header
